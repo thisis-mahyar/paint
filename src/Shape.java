888d92f4bda27c5906b8f83e0671fa7505cdb114
Mahyar <mahyarstate@gmail.com>
2024-09-06 20:16:41 +0330

    [v2] transfer extendable parts to Shape.java, a little change in diagram.html

--- src/Shape.java
+++ src/Shape.java
@@ -1,2 +1,31 @@
 public class Shape {
+    public String name;
+    public String borderColor;
+    public String fillColor;
+    public boolean isFilled;
+
+    public Shape() {
+        this.borderColor = "Black";
+        this.fillColor = "White";
+        this.isFilled = false;
+    }
+
+    public Shape(String borderColor, String fillColor) {
+        this.borderColor = borderColor;
+        this.fillColor = fillColor;
+        this.isFilled = false;
+    }
+
+    public void draw() {
+    }
+
+    public void move(int xMove, int yMove) {
+    }
+
+    public void erase() {
+    }
+
+    public boolean hit(Point p) {
+        return false;
+    }
 }
