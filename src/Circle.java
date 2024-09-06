b9b9c7eb0853d33d6f3dc7051d0e12b48ee7fcc9
Mahyar <mahyarstate@gmail.com>
2024-09-06 22:17:49 +0330

    [v4] Circle.java extends Oval.java

--- src/Circle.java
+++ src/Circle.java
@@ -1,10 +1,8 @@
-public class Circle extends Shape {
-    public Point center;
-    public int radius;
-
+public class Circle extends Oval {
     public Circle() {
         this.center = new Point();
-        this.radius = 1;
+        this.axis1 = 1;
+        this.axis2 = 2;
         this.name = "circle-" + ((int) (Math.random() * 1000)); // a random id
         this.borderColor = "Black";
         this.fillColor = "White";
@@ -12,30 +10,10 @@ public class Circle extends Shape {
 
     public Circle(Point center, int radius, String borderColor, String fillColor) {
         this.center = center;
-        this.radius = radius;
+        this.axis1 = radius;
+        this.axis2 = radius;
         this.name = "circle-" + ((int) (Math.random() * 1000)); // a random id
         this.borderColor = borderColor;
         this.fillColor = fillColor;
-    }
-
-    @Override
-    public void draw() { // yet just a message
-        System.out.format("draw %s from %s with radius = %d.\n", name, center.toString(), radius);
-    }
-
-    @Override
-    public void move(int xMove, int yMove) {
-        center.setX(center.getX() + xMove);
-        center.setY(center.getY() + yMove);
-    }
-
-    @Override
-    public void erase() { // yet just a message
-        System.out.format("erase %s.\n", name);
-    }
-
-    @Override
-    public boolean hit(Point p) {
-        return center.distance(p) <= radius;
     }
 }
