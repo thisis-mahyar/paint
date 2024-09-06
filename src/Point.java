1a396082f26d2e1fd3b82918eb8d725a19b1a17f
Mahyar <mahyarstate@gmail.com>
2024-09-06 17:15:09 +0330

    [v1] add fields, constructors, getters, setters

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,37 @@
 public class Point {
+    private int x;
+    private int y;
+
+    public Point() {
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int[] getXY() {
+        return new int[]{x, y};
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
