192a82eb9b0e9242f65337f4afd7bc40f02fa774
Mahyar <mahyarstate@gmail.com>
2024-09-06 17:15:58 +0330

    [v1] add toString(), distance()

--- src/Point.java
+++ src/Point.java
@@ -34,4 +34,21 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    @Override
+    public String toString() {
+        return String.format("(%d,%d)", x, y);
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt(Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2));
+    }
+
+    public double distance(Point another) {
+        return distance(another.x, another.y);
+    }
+
+    public double distance() {
+        return distance(0, 0);
+    }
 }
