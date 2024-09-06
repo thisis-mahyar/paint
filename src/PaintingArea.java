1a3c95f5522aea3b788a64b1c57430a67274abf5
Mahyar <mahyarstate@gmail.com>
2024-09-06 18:46:05 +0330

    [v1] add fields, constructor (!) rather than initializing the arrays by null, I newed them inside the constructor.

--- src/PaintingArea.java
+++ src/PaintingArea.java
@@ -1,2 +1,11 @@
 public class PaintingArea {
+    public Line[] lineArr;
+    public Rectangle[] rectArr;
+    public Circle[] circleArr;
+
+    public PaintingArea() {
+        lineArr = new Line[0];
+        rectArr = new Rectangle[0];
+        circleArr = new Circle[0];
+    }
 }
