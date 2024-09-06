80b6dba4dbb95974a600c0f9ab419d4be4874441
Mahyar <mahyarstate@gmail.com>
2024-09-06 23:49:25 +0330

    [v4] update draw()

--- src/Polygon.java
+++ src/Polygon.java
@@ -20,7 +20,23 @@ public class Polygon extends Shape {
     }
 
     @Override
-    public void draw(Graphics g) { // yet just a message
+    public void draw(Graphics g) {
+        int[] xPoints = new int[points.size()];
+        int[] yPoints = new int[points.size()];
+
+        for (int i = 0; i < points.size(); i++) {
+            xPoints[i] = points.get(i).getX();
+            yPoints[i] = points.get(i).getY();
+        }
+
+        if (!isFilled) {
+            g.setColor(borderColor);
+            g.drawPolygon(xPoints, yPoints, points.size());
+        } else {
+            g.setColor(fillColor);
+            g.drawPolygon(xPoints, yPoints, points.size());
+        }
+
         System.out.format("draw %s with %d points.\n", name, points.size());
     }
 
@@ -39,7 +55,7 @@ public class Polygon extends Shape {
 
     @Override
     public boolean hit(Point p) {
-        // And advanced math formula
+        // an advanced math formula
 
         int xMin = points.get(0).getX();
         int xMax = points.get(0).getX();
