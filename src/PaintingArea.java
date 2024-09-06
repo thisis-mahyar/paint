75305d5dce14214425d8eb84ef40840d43e9a154
Mahyar <mahyarstate@gmail.com>
2024-09-07 00:11:03 +0330

    [v4] add Graphics, width, height, redrawAll() and update status()

--- src/PaintingArea.java
+++ src/PaintingArea.java
@@ -1,16 +1,22 @@
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PaintingArea {
     List<Shape> shapeList;
+    Graphics g;
+    int width, height;
 
-    public PaintingArea() {
+    public PaintingArea(Graphics g, int width, int height) {
         shapeList = new ArrayList<>();
+        this.g = g;
+        this.width = width;
+        this.height = height;
     }
 
     public void addShape(Shape s) {
         shapeList.add(s);
-        s.draw();
+        s.draw(g);
     }
 
     public boolean removeShape(int index) {
@@ -22,7 +28,7 @@ public class PaintingArea {
     }
 
     public int indexOfLine(Shape s) {
-       return shapeList.indexOf(s);
+        return shapeList.indexOf(s);
     }
 
     public Shape getShapeByPoint(Point p) {
@@ -36,22 +42,58 @@ public class PaintingArea {
     public void moveShape(Point p, int xMove, int yMove) {
         Shape s = getShapeByPoint(p);
 
-        if (s != null)
+        if (s != null) {
             s.move(xMove, yMove);
+            reDrawAll();
+        }
     }
 
     public void status() {
-        int lCount = 0, cCount = 0, rCount = 0;
+        int lines = 0;
+        int circles = 0;
+        int rectangles = 0;
+        int ovals = 0;
+        int squares = 0;
+        int polygons = 0;
+        int triangles = 0;
 
         for (Shape s : shapeList) {
-            if (s instanceof Line)
-                lCount++;
-            else if (s instanceof Circle)
-                cCount++;
-            else if (s instanceof Rectangle)
-                rCount++;
+            if (s instanceof Line) {
+                lines++;
+            } else if (s instanceof Oval) {
+                if (s instanceof Circle)
+                    circles++;
+                else
+                    ovals++;
+            } else if (s instanceof Rectangle) {
+                if (s instanceof Square)
+                    squares++;
+                else
+                    rectangles++;
+            } else if (s instanceof Polygon) {
+                if (s instanceof Triangle)
+                    triangles++;
+                else
+                    polygons++;
+            }
         }
 
-        System.out.format("Line = %d, Circle = %d, Rectangle = %d\n", lCount, cCount, rCount);
+        System.out.format("# lines      = %d\n", lines);
+        System.out.format("# ovals      = %d\n", ovals);
+        System.out.format("# circles    = %d\n", circles);
+        System.out.format("# rectangles = %d\n", rectangles);
+        System.out.format("# squares    = %d\n", squares);
+        System.out.format("# polygons   = %d\n", polygons);
+        System.out.format("# triangles  = %d\n", triangles);
+    }
+
+    public void reDrawAll() {
+        // first you kinda erase the board by putting a white rectangle on it!
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, width, height);
+
+        // then you redraw all the current shapes
+        for (Shape s : shapeList)
+            s.draw(g);
     }
 }
