509f182d066a4bd15655dfaa62bb67c6c01303b1
Mahyar <mahyarstate@gmail.com>
2024-09-06 20:19:20 +0330

    [v2] changes in Line.java due to the inheritance

--- src/Line.java
+++ src/Line.java
@@ -1,7 +1,5 @@
 public class Line extends Shape {
     public Point p1, p2;
-    public String name;
-    public String borderColor;
 
     public Line() {
         this.p1 = new Point();
@@ -17,10 +15,12 @@ public class Line extends Shape {
         this.borderColor = borderColor;
     }
 
+    @Override
     public void draw() { // yet just a message
         System.out.format("draw %s from %s to %s.\n", name, p1.toString(), p2.toString());
     }
 
+    @Override
     public void move(int xMove, int yMove) {
         p1.setX(p1.getX() + xMove);
         p1.setY(p1.getY() + yMove);
@@ -28,10 +28,12 @@ public class Line extends Shape {
         p2.setY(p2.getY() + yMove);
     }
 
+    @Override
     public void erase() { // yet just a message
         System.out.format("erase %s.\n", name);
     }
 
+    @Override
     public boolean hit(Point p) {
         return p1.distance(p2) == p1.distance(p) + p2.distance(p);
     }
