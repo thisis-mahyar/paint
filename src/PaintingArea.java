b3e2fcd0fb6488cd2993a86b355e8037bb428077
Mahyar <mahyarstate@gmail.com>
2024-09-06 18:47:12 +0330

    [v1] add add()

--- src/PaintingArea.java
+++ src/PaintingArea.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class PaintingArea {
     public Line[] lineArr;
     public Rectangle[] rectArr;
@@ -7,5 +9,23 @@ public class PaintingArea {
         lineArr = new Line[0];
         rectArr = new Rectangle[0];
         circleArr = new Circle[0];
+    }
+
+    public void addLine(Line l) {
+        lineArr = Arrays.copyOf(lineArr, lineArr.length + 1);
+        lineArr[lineArr.length - 1] = l;
+        l.draw();
+    }
+
+    public void addCircle(Circle c) {
+        circleArr = Arrays.copyOf(circleArr, circleArr.length + 1);
+        circleArr[circleArr.length - 1] = c;
+        c.draw();
+    }
+
+    public void addRectangle(Rectangle r) {
+        rectArr = Arrays.copyOf(rectArr, rectArr.length + 1);
+        rectArr[rectArr.length - 1] = r;
+        r.draw();
     }
 }
