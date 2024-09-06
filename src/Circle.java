f0ba04caa5bccaa38ede48e90b7f4e052e8e4d69
Mahyar <mahyarstate@gmail.com>
2024-09-06 17:32:53 +0330

    [v1] add fields, constructors

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,23 @@
 public class Circle {
+    public Point center;
+    public int radius;
+    public String name;
+    public String borderColor;
+    public String fillColor;
+
+    public Circle() {
+        this.center = new Point();
+        this.radius = 1;
+        this.name = "circle-" + ((int) (Math.random() * 1000)); // a random id
+        this.borderColor = "Black";
+        this.fillColor = "White";
+    }
+
+    public Circle(Point center, int radius, String borderColor, String fillColor) {
+        this.center = center;
+        this.radius = radius;
+        this.name = "circle-" + ((int) (Math.random() * 1000)); // a random id
+        this.borderColor = borderColor;
+        this.fillColor = fillColor;
+    }
 }
