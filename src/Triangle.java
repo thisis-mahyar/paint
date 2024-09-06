1d06a8efba2a665ea44c95d83a7d39df6dcea02c
Mahyar <mahyarstate@gmail.com>
2024-09-06 22:53:30 +0330

    [v4] Triangle.java extends Polygon.java

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,2 +1,22 @@
-public class Triangle {
+import java.util.ArrayList;
+
+public class Triangle extends Polygon {
+    public Triangle() {
+        this.points = new ArrayList<>(3);
+        this.name = "triangle-" + ((int) (Math.random() * 1000)); // a random id
+    }
+
+    public Triangle(Point p1, Point p2, Point p3, String borderColor, String fillColor) {
+        this.points.set(0, p1);
+        this.points.set(1, p2);
+        this.points.set(2, p3);
+        this.name = "triangle-" + ((int) (Math.random() * 1000)); // a random id
+        this.borderColor = borderColor;
+        this.fillColor = fillColor;
+    }
+
+    @Override
+    public void draw() { // yet just a message
+        System.out.format("draw %s with points: %s, %s, %s.\n", name, points.get(0).toString(), points.get(1).toString(), points.get(2).toString());
+    }
 }
