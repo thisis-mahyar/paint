d83a8ce354554ed4cd15c966315d05520cc9b868
Mahyar <mahyarstate@gmail.com>
2024-09-06 22:11:14 +0330

    [v4] add fields, constructors, extended methods

--- src/Oval.java
+++ src/Oval.java
@@ -1,2 +1,45 @@
-public class Oval {
+public class Oval extends Shape {
+    public Point center;
+    public int axis1, axis2;
+
+    public Oval() {
+        this.center = new Point();
+        this.axis1 = 1;
+        this.axis2 = 1;
+        this.name = "oval-" + ((int) (Math.random() * 1000)); // a random id
+        this.borderColor = "Black";
+        this.fillColor = "White";
+    }
+
+    public Oval(Point center, int axis1, int axis2, String borderColor, String fillColor) {
+        this.center = center;
+        this.axis1 = axis1;
+        this.axis2 = axis2;
+        this.name = "oval-" + ((int) (Math.random() * 1000)); // a random id
+        this.borderColor = borderColor;
+        this.fillColor = fillColor;
+    }
+
+    @Override
+    public void draw() { // yet just a message
+        System.out.format("draw %s from %s with axis1 = %d, axis2 = %d.\n", name, center.toString(), axis1, axis2);
+    }
+
+    @Override
+    public void move(int xMove, int yMove) {
+        center.setX(center.getX() + xMove);
+        center.setY(center.getY() + yMove);
+    }
+
+    @Override
+    public void erase() { // yet just a message
+        System.out.format("erase %s.\n", name);
+    }
+
+    @Override
+    public boolean hit(Point p) {
+        // An advanced math formula
+        double result = Math.pow((p.getX() - center.getX()), 2 / Math.pow(axis1, 2)) + Math.pow((p.getY() - center.getY()), 2) / Math.pow(axis2, 2);
+        return result <= 1;
+    }
 }
