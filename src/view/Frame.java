7c9b1e141ea494227abeabc5faa9f58f2c02daf4
Mahyar <mahyarstate@gmail.com>
2025-02-24 02:06:28 +0330

    [v4] integrate and deploy the app

--- src/view/Frame.java
+++ src/view/Frame.java
@@ -1,7 +1,25 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Frame extends JFrame {
+    public Frame() {
+        setTitle("Paint");
+        setSize(800, 600);
 
+        Menu menu = new Menu();
+        PaintingArea paintingArea = new PaintingArea(menu);
+
+        getContentPane().setLayout(new BorderLayout());
+
+        add(menu, BorderLayout.WEST);
+        add(paintingArea, BorderLayout.CENTER);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Frame().setVisible(true));
+    }
 }
