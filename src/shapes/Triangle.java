4163275f6ac9be0acef823976d983bacb56a8e27
Mahyar <mahyarstate@gmail.com>
2025-02-23 16:54:46 +0330

    [v4] fix bug

--- src/shapes/Triangle.java
+++ src/shapes/Triangle.java
@@ -5,14 +5,14 @@ import java.util.ArrayList;
 
 public class Triangle extends Polygon {
     public Triangle() {
-        this.points = new ArrayList<>(3);
+        this.points = new ArrayList<>();
         this.name = "triangle-" + ((int) (Math.random() * 1000)); // a random id
     }
 
     public Triangle(Point p1, Point p2, Point p3, Color borderColor, Color fillColor) {
-        this.points.set(0, p1);
-        this.points.set(1, p2);
-        this.points.set(2, p3);
+        this.points.add(p1);
+        this.points.add(p2);
+        this.points.add(p3);
         this.name = "triangle-" + ((int) (Math.random() * 1000)); // a random id
         this.borderColor = borderColor;
         this.fillColor = fillColor;
